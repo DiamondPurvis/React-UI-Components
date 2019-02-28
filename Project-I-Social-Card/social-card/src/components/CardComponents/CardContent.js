db2af73d9785d3562fb53c4995cd92d0d80db00b
DiamondPurvis <purvisdiamond@gmail.com>
2019-02-27 23:34:02 -0500

    Set card container -imported banner -imported content

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Card.css';
+
+
+function CardContent() {
+    return (
+        <div className="card-content">
+        <h1>Getting started with React</h1>
+        <p>Rect makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+        <a href="https://www.reactjs.org"> reactjs.org</a>
+        </div>
+    );
+}
+
+export default CardContent;
