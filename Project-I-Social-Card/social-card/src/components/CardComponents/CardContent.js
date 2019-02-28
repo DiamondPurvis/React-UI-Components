ab825b6668643f829b49b8d70cc188997964588f
DiamondPurvis <purvisdiamond@gmail.com>
2019-02-28 00:44:06 -0500

    Added styling -app.css -card.css -header.css -fixed react typo

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -6,8 +6,8 @@ function CardContent() {
     return (
         <div className="card-content">
         <h1>Getting started with React</h1>
-        <p>Rect makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
-        <a href="https://www.reactjs.org"> reactjs.org</a>
+        <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+        <a href="https://reactjs.org/"> reactjs.org</a>
         </div>
     );
 }
